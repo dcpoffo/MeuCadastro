db5d5d3a90992869aecbebd5065a5191f9a7f437
Darlan <dcpoffo@gmail.com>
2020-07-17 19:23:40 -0300

    scripts

---
+++ InsertClientes.sql
@@ -0,0 +1,3 @@
+insert into cliente (nome) values ('Thor');
+insert into cliente (nome) values ('Howe');
+insert into cliente (nome) values ('Lam');
---
+++ InsertRNC.sql
@@ -0,0 +1,6 @@
+insert into rnc values (27,2,'2020-06-30 00:00:00.0000000');
+insert into rnc values (27,2,'2020-07-02 00:00:00.0000000');
+insert into rnc values (27,3,'2020-07-06 00:00:00.0000000');
+insert into rnc values (27,3,'2020-06-26 00:00:00.0000000');
+insert into rnc values (3,2,'2020-07-06 00:00:00.0000000');
+insert into rnc values (3,4,'2020-07-13 00:00:00.0000000');
